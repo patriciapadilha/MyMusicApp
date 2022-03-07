f2a7857b580598ea2a5972e8647a43120efca4af
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-03-07 17:31:55 -0300

    Requisito 4 - links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -36,6 +37,9 @@ class Header extends React.Component {
               { userName }
             </p>
           )}
+        <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
       </header>
     );
   }
