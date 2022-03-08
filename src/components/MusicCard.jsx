0534d6b4167a83c2c7c9eeddfe10efd2a0397548
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-03-08 17:28:03 -0300

    Requisito 8 - Favoritar músicas

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      isChecked: false,
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.getAddRemoveSong = this.getAddRemoveSong.bind(this);
+  }
+
+  onInputChange({ target: { checked } }) {
+    this.setState({ isChecked: checked }, () => this.getAddRemoveSong());
+  }
+
+  async getAddRemoveSong() {
+    const { music } = this.props;
+    const { isChecked } = this.state;
+    this.setState({
+      loading: true,
+    });
+    if (isChecked) {
+      await addSong(music);
+    } else {
+      await removeSong(music);
+    }
+    this.setState({
+      loading: false,
+    });
+  }
+
   render() {
     const { music } = this.props;
+    const { isChecked, loading } = this.state;
     return (
       <div
         key={ music.trackName }
@@ -16,6 +49,20 @@ class MusicCards extends React.Component {
             <code>audio</code>
             .
           </audio>
+          {loading ? <Loading />
+            : (
+              <label htmlFor="favorite">
+                Favorita
+                <input
+                  type="checkbox"
+                  id="favorite"
+                  name="favorite"
+                  data-testid={ `checkbox-music-${music.trackId}` }
+                  checked={ isChecked }
+                  onChange={ this.onInputChange }
+                />
+              </label>
+            )}
         </div>
       </div>
     );
