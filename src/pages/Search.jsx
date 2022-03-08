aa4acb040c6dddcea9eaec3da3592320fea3ee07
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-03-08 13:09:59 -0300

    Requisito 6 - requisição albuns

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends React.Component {
   constructor() {
@@ -7,9 +10,16 @@ class Search extends React.Component {
 
     this.onInputChange = this.onInputChange.bind(this);
     this.validateSearch = this.validateSearch.bind(this);
+    this.searchRequest = this.searchRequest.bind(this);
+
     this.state = {
       artist: '',
+      searchArtist: '',
       searchIsDisabled: true,
+      loading: false,
+      apiRequest: false,
+      albuns: [],
+      isNotEmpty: false,
     };
   }
 
@@ -28,30 +38,80 @@ class Search extends React.Component {
     }
   }
 
+  async searchRequest() {
+    const { artist } = this.state;
+    this.setState({ loading: true });
+    const response = await searchAlbumsAPI(artist);
+    console.log(response);
+    this.setState({
+      loading: false,
+      apiRequest: true,
+      albuns: response,
+      searchArtist: artist,
+      artist: '',
+      isNotEmpty: response.length === 0,
+    });
+  }
+
   render() {
-    const { artist, searchIsDisabled } = this.state;
+    const {
+      artist,
+      searchIsDisabled,
+      loading,
+      apiRequest,
+      albuns,
+      searchArtist,
+      isNotEmpty,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         Pagina Search
-        <label htmlFor="search-artist">
-          <input
-            type="text"
-            placeholder="Buscar artista"
-            id="search-artist"
-            name="artist"
-            data-testid="search-artist-input"
-            onChange={ this.onInputChange }
-            value={ artist }
-          />
-        </label>
-        <button
-          disabled={ searchIsDisabled }
-          type="button"
-          data-testid="search-artist-button"
-        >
-          Entrar
-        </button>
+        {loading ? <Loading /> : (
+          <div>
+            {apiRequest && <p>{`Resultado de álbuns de: ${searchArtist}`}</p>}
+            <label htmlFor="search-artist">
+              <input
+                type="text"
+                placeholder="Buscar artista"
+                id="search-artist"
+                name="artist"
+                data-testid="search-artist-input"
+                onChange={ this.onInputChange }
+                value={ artist }
+              />
+            </label>
+            <button
+              disabled={ searchIsDisabled }
+              type="button"
+              data-testid="search-artist-button"
+              onClick={ this.searchRequest }
+            >
+              Pesquisar
+            </button>
+            <div>
+              {isNotEmpty ? <p>Nenhum álbum foi encontrado</p> : (
+                albuns
+                  .map((album) => (
+                    <div
+                      className="album-list"
+                      key={ album.collectionId }
+                    >
+                      <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                      <Link
+                        to={ `/album/${album.collectionId}` }
+                        data-testid={ `link-to-album-${album.collectionId}` }
+                      >
+                        { album.collectionName }
+                      </Link>
+                    </div>
+                  ))
+              )}
+            </div>
+          </div>
+        )}
+
+
       </div>
     );
   }
