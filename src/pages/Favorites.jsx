cced0fc73bedd83cddad0ed77fd317d08b937fb2
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-03-08 18:35:32 -0300

    reset

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 
 class Favorites extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class Favorites extends React.Component {
       <div data-testid="page-favorites">
         <Header />
         Pagina Favorites
+        <Loading />
       </div>
     );
   }
